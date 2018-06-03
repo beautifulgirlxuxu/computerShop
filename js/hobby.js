d6d37673902f2e31e4edf716d619b0ca45ede521
beautifulgirlxuxu <804316947@qq.com>
2018-06-03 23:34:26 +0800

    电脑海淘商城

--- js/hobby.js
+++
@@ -1,7 +0,0 @@
-var selTag = document.getElementsByClassName('selected');
-// console.log(selTag);
-for(let i = 0, len = selTag.length; i<len; i++) {
-    selTag[i].addEventListener('click', function() {
-        selTag[i].style.backgroundColor = 'grey';
-    },true)
-}
